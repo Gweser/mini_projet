cd7b78071869172fe8e2aab280ad6584d99e985e
SEVENO Enzo & RIOU LE DROGO Dylan <seveno.e@stjolorient.fr>
2025-01-23 10:49:46 +0100

    Modification du client.

--- UDP/client_udp_ping_pong.c
+++ UDP/client_udp_ping_pong.c
@@ -1,22 +1,55 @@
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
 #include "udp.h"
 #include "erreur.h"
 #include "nombre.h"
-
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <netinet/in.h>
 
-int traiter_commande_wrapper(int argc, char** argv) {
-    traiter_commande(argc, argv, "Usage: <adresse IP> <port> <message>\nMauvais nombre d'arguments");
-    traiter_commande(argc, argv, "Usage: <adresse IP> <port> <message>\n<adresse IP> est une adresse IP au format décimal pointé");
-    traiter_commande(argc, argv, "Usage: <adresse IP> <port> <message>\n<port> est un port non réservé");
-    return 0;
+/** Montage de la fixation - appelé avant chaque cas de test. **/
+int init_suite(void) { return 0; }
+
+/* Démontage de la fixation - appelé après chaque cas de test. */
+int clean_suite(void) { return 0; }
+
+/****** Cas de test - envoi de message UDP *****/
+void test_envoi_message(void) {
+    CU_ASSERT_EQUAL(send_udp_message("127.0.0.1", 8080, "Test message"), 0);
 }
 
-int main(int argc, char** argv) {
-    traiter_commande_wrapper(argc, argv);
+/******************* Lancement des tests ***********************/
+int main ( void )
+{
+   CU_pSuite pSuite = NULL;
+   unsigned int status = 0;
 
+   /* initialisation des test CUnit */
+   if ( CUE_SUCCESS != CU_initialize_registry() )
+      return CU_get_error();
 
-    exit(0);
+   /* ajout de la suite de test */
+   pSuite = CU_add_suite( "Test module client", init_suite, clean_suite );
+   if ( NULL == pSuite ) {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* ajout des cas de test dans la suite de test */
+   if ( (NULL == CU_add_test(pSuite, "Cas de test - envoi de message UDP", test_envoi_message))
+      )
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+
+   /* lancement de tous les tests avec l'interface de base */
+   CU_basic_set_mode(CU_BRM_NORMAL);
+   if ( CUE_SUCCESS != CU_basic_run_tests() )
+      return CU_get_error();
+   
+   status = CU_get_number_of_tests_failed();
+   CU_basic_show_failures(CU_get_failure_list());
+  
+   /* cloture des tests */
+   CU_cleanup_registry();
+   return status;
 }
