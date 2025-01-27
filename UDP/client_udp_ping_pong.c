cba37f306f556b35264e86b2b88d147b8505a116
User1 <riouledrogo.d@stjolorient.fr>
2025-01-27 11:35:51 +0100

    client_udp_ping_pong.c

--- UDP/client_udp_ping_pong.c
+++ UDP/client_udp_ping_pong.c
@@ -4,20 +4,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <string.h>
 #include <netinet/in.h>
 
-int traiter_commande_wrapper(int argc, char* argv) {
-	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\nmauvais nombre d'aguments");	
-	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\n<adresse IP> est une adresse IP au format décimal pointé");
-	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\n<port> est un port non réservé");
-	
+int traiter_commande_wrapper(int argc, char** argv) {
+    traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\nmauvais nombre d'aguments");    
+    traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\n<adresse IP> est une adresse IP au format décimal pointé");
+    traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\n<port> est un port non réservé");
+    return 0;
 }
 
 int main(int argc, char** argv) {
-	
-	traiter_commande_wrapper(argc, argv);
-
+    traiter_commande_wrapper(argc, argv);
+    return 0;
 }
-
-
