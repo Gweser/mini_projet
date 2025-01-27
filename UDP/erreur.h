1ec64a2d3120605c437dfb17e0746837fb4b496c
Gweser <seveno.e@stjolorient.fr>
2025-01-27 11:11:42 +0100

    Remise en place du fichier erreur.h !

--- UDP/erreur.h
+++ UDP/erreur.h
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 #ifndef _ERREUR_H
 #define _ERREUR_H
 void traiter_erreur(const char *);
 void traiter_commande(int, const char *, const char *);
 int verbose_mode(void);
 #endif // _ERREUR_H_
-=======
-#ifndef ERREUR_H
-#define ERREUR_H
-
-void traiter_commande(int, const char * , const char *);
-
-#endif
->>>>>>> c9a65f8a7616181b10e2eb9d441317655519db3d
