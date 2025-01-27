8dfe6b1e5cb43ea6a508247b10a3cfe1bbe98e38
SEVENO Enzo & RIOU LE DROGO Dylan <seveno.e@stjolorient.fr>
2025-01-27 10:28:23 +0100

    Modification et mise à jour du fichier.

--- UDP/client_udp_ping_pong.c
+++ UDP/client_udp_ping_pong.c
@@ -5,16 +5,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <netinet/in.h>
 
-void traiter_commande_wrapper(int argc, char** argv) {
-    traiter_commande_wrapper(argc != 4, argv[0], "<adresse IP> <port> <message>\nMauvais nombre d'arguments");
-    traiter_commande_wrapper(argc != 4, argv[0], "<adresse IP> <port> <message>\n<adresse IP> est une adresse IP au format décimal pointé");
-    traiter_commande_wrapper(argc != 4, argv[0], "<adresse IP> <port> <message>\n<port> est un port non réservé");
+int traiter_commande_wrapper(int argc, char** argv) {
+	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\nmauvais nombre d'aguments");	
+	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\n<adresse IP> est une adresse IP au format décimal pointé");
+	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\n<port> est un port non réservé");
+	
 }
 
 int main(int argc, char** argv) {
-    traiter_commande_wrapper(argc, argv);
+	
+	traiter_commande_wrapper(argc, argv);
 
-    exit(0);
 }
--- UDP/erreur.c
+++ UDP/erreur.c
@@ -1,29 +1,28 @@
+#include "erreur.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-#include "erreur.h"
-
 
 /* Afficher une éventuelle erreur et quitter */
 void traiter_erreur(const char * msg) {
-	if (errno) {
-		perror(msg);
-		exit(1);
-	}
+    if (errno) {
+        perror(msg);
+        exit(1);
+    }
 }
 
 /* Verifier le ligne de commande */
-void traiter_commande_wrapper(int erreur, const char * cmd, const char * arg_desc) {
-	if (erreur) {
-  		fprintf(stderr, "Ligne de commande invalide\n");
-  		fprintf(stderr, "Usage : %s %s\n", cmd, arg_desc);
-		exit(1);
-	}
+void traiter_commande(int erreur, const char * cmd, const char * arg_desc) {
+    if (erreur) {
+        fprintf(stderr, "Ligne de commande invalide\n");
+        fprintf(stderr, "Usage : %s %s\n", cmd, arg_desc);
+        exit(1);
+    }
 }
 
 /* Traiter le mode verbose */
 int verbose_mode() {
-	return (getenv("VERBOSE") != NULL && \
-		strcmp(getenv("VERBOSE"), "1") == 0);
+    return (getenv("VERBOSE") != NULL && \
+        strcmp(getenv("VERBOSE"), "1") == 0);
 }
