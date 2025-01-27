59e633e3974dc5fbf128529f4cc3156fb9c1c6d4
User1 <riouledrogo.d@stjolorient.fr>
2025-01-27 11:58:42 +0100

    Modification du fichier serveur_udp_ping_pong.c

--- UDP/serveur_udp_ping_pong.c
+++ UDP/serveur_udp_ping_pong.c
@@ -6,16 +6,13 @@
 #include <string.h>
 #include <netinet/in.h>
 
-
 int traiter_commande_wrapper(int argc, char** argv) {
-    traiter_commande(argc, argv, "Usage: <port>\nMauvais nombre d'arguments");
-    traiter_commande(argc, argv, "Usage: <port>\n<port> est un port non réservé");
+    traiter_commande(argc, argv[0], "Usage: <port>\nMauvais nombre d'arguments");
+    traiter_commande(argc, argv[0], "Usage: <port>\n<port> est un port non réservé");
     return 0;
 }
 
 int main(int argc, char** argv) {
     traiter_commande_wrapper(argc, argv);
-
-
-    exit(0);
+    return 0;
 }
