7b497d64d9d730c9145c50b0de376337f163cdbe
SEVENO Enzo & RIOU LE DROGO Dylan <seveno.e@stjolorient.fr>
2025-01-27 09:38:41 +0100

    Modification du client UDP !

--- UDP/client_udp_ping_pong.c
+++ UDP/client_udp_ping_pong.c
@@ -7,13 +7,13 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 
+
 void traiter_commande(int, char **, const char *);
 
-int traiter_commande_wrapper(int argc, char** argv) {
+void traiter_commande_wrapper(int argc, char** argv) {
     traiter_commande(argc, argv, "Usage: <adresse IP> <port> <message>\nMauvais nombre d'arguments");
     traiter_commande(argc, argv, "Usage: <adresse IP> <port> <message>\n<adresse IP> est une adresse IP au format décimal pointé");
     traiter_commande(argc, argv, "Usage: <adresse IP> <port> <message>\n<port> est un port non réservé");
-    return 0;
 }
 
 int main(int argc, char** argv) {
