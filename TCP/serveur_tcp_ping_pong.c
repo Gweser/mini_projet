ba942de38cfb972c44634a996faef1b5032443e3
User1 <riouledrogo.d@stjolorient.fr>
2025-01-27 11:21:03 +0100

    Modification fichier

--- TCP/serveur_tcp_ping_pong.c
+++ TCP/serveur_tcp_ping_pong.c
@@ -1,22 +1,23 @@
 #include "udp.h"
 #include "erreur.h"
 #include "nombre.h"
-
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <string.h>
 #include <netinet/in.h>
 
-
 int traiter_commande_wrapper(int argc, char** argv) {
-    traiter_commande(argc, argv, "Usage: <port>\nMauvais nombre d'arguments");
-    traiter_commande(argc, argv, "Usage: <port>\n<port> est un port non réservé");
-    return 0;
+	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\nmauvais nombre d'aguments");	
+	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\n<adresse IP> est une adresse IP au format décimal pointé");
+	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\n<port> est un port non réservé");
+	
 }
 
 int main(int argc, char** argv) {
-    traiter_commande_wrapper(argc, argv);
+	
+	traiter_commande_wrapper(argc, argv);
 
-
-    exit(0);
 }
+
+
