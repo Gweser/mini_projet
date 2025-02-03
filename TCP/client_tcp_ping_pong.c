e33defba10103749f4d38aede612059b5c72be02
SEVENO Enzo & RIOU LE DROGO Dylan <seveno.e@stjolorient.fr>
2025-02-03 11:28:40 +0100

    Mise en place du client tcp

--- TCP/client_tcp_ping_pong.c
+++ TCP/client_tcp_ping_pong.c
@@ -4,20 +4,54 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <string.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
-int traiter_commande_wrapper(int argc, char** argv) {
-	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\nmauvais nombre d'aguments");	
-	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\n<adresse IP> est une adresse IP au format décimal pointé");
-	traiter_commande(argc, argv[0], "<adresse IP> <port> <message>\n<port> est un port non réservé");
-	
+// Fonction pour vérifier si une chaîne est une adresse IP valide
+int is_valid_ip(const char* ip) {
+    struct sockaddr_in sa;
+    return inet_pton(AF_INET, ip, &(sa.sin_addr)) == 1;
+}
+
+void traiter_commande_wrapper(int argc, char** argv) {
+    // Vérifier le nombre d'arguments
+    traiter_commande(argc != 3, argv[0], 
+        "<adresse IP> <port>\nmauvais nombre d'aguments");
+
+    // Vérifier le format de l'adresse IP
+    traiter_commande(!is_valid_ip(argv[1]), argv[0], 
+        "<adresse IP> <port>\n<adresse IP> est une adresse IP au format décimal pointé");
+
+    // Vérifier que le port est valide 
+    int port = atoi(argv[2]);
+    traiter_commande(port < 1024 || port > 65535, argv[0], 
+        "<adresse IP> <port>\n<port> est un port non réservé");
 }
 
 int main(int argc, char** argv) {
 	
-	traiter_commande_wrapper(argc, argv);
-
+    traiter_commande_wrapper(argc, argv);
+    
+    // Récupérer les arguments
+    char* adresseIP = argv[1];
+    int port = atoi(argv[2]);
+    
+    // Créer la socket client
+    SOCK socket_client;
+    creer_socket(adresseIP, port, &socket_client);
+    
+    // Envoyer le message "ping"
+    char* message = "ping";
+    envoyer_message(&socket_client, message);
+    printf("Message envoyé : %s\n", message);
+    
+    // Attendre la réponse "pong"
+    char buffer[TAILLE_BUFFER];
+    recevoir_message(&socket_client, buffer);
+    printf("Réponse reçue : %s\n", buffer);
+    
+    // Fermer la connexion
+    fermer_connexion(&socket_client);
+    
+    return 0;
 }
-
-
