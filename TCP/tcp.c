73ea822cad61716428b9533a7a6b701ac8803b56
SEVENO Enzo & RIOU LE DROGO Dylan <seveno.e@stjolorient.fr>
2025-02-03 11:10:27 +0100

    Modification du tcp.c

--- TCP/tcp.c
+++ TCP/tcp.c
@@ -1,97 +1,84 @@
-#include "erreur.h"
 #include "tcp.h"
-
+#include "erreur.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+
+static struct sockaddr_in serv_addr, cli_addr;
+static socklen_t clilen;
 
 int creer_socket(char* adresseIP, int port) {
-    int sock;
-    int ip;
-    
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-        traiter_erreur(__FUNCTION__);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        traiter_erreur("Erreur lors de la création de la socket");
     }
 
-    ip = inet_addr(adresseIP);
-    if (ip == -1) {
-        traiter_erreur(__FUNCTION__);
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+
+    if (adresseIP == NULL) {
+        serv_addr.sin_addr.s_addr = INADDR_ANY;
+    } else {
+        if (inet_pton(AF_INET, adresseIP, &serv_addr.sin_addr) <= 0) {
+            traiter_erreur("Adresse IP invalide");
+        }
     }
 
-    if (connect(sock, (struct sockaddr *)&ip, sizeof(ip)) == -1) {
-        traiter_erreur(__FUNCTION__);
-    }
-
-    return sock;
+    return sockfd;
 }
 
-void connecter_socket(int sock, char* adresseIP, int port) {
-    int ip;
-    
-    ip = inet_addr(adresseIP);
-    if (ip == -1) {
-        traiter_erreur(__FUNCTION__);
-    }
-
-    if (connect(sock, (struct sockaddr *)&ip, sizeof(ip)) == -1) {
-        traiter_erreur(__FUNCTION__);
+void connecter_socket(int sockfd) {
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        traiter_erreur("Erreur lors de la connexion de la socket");
     }
 }
 
-void attacher_socket(int sock, int port) {
-    int ip;
-    
-    ip = INADDR_ANY;
-
-    if (bind(sock, (struct sockaddr *)&ip, sizeof(ip)) == -1) {
-        traiter_erreur(__FUNCTION__);
+void attacher_socket(int sockfd) {
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        traiter_erreur("Erreur lors de l'attachement de la socket");
     }
 }
 
-void dimensionner_file_attente_socket(int sock, int taille) {
-    if (listen(sock, taille) == -1) {
-        traiter_erreur(__FUNCTION__);
+void dimensionner_file_attente_socket(int sockfd, int taille) {
+    if (listen(sockfd, taille) < 0) {
+        traiter_erreur("Erreur lors de l'écoute des connexions");
     }
 }
 
-void init_addr_client(int sock) {
+void init_addr_client() {
+    clilen = sizeof(cli_addr);
+    memset(&cli_addr, 0, clilen);
 }
 
-int attendre_connexion(int sock) {
-    int socktraitement;
-    
-    socktraitement = accept(sock, NULL, NULL);
-    if (socktraitement == -1) {
-        traiter_erreur(__FUNCTION__);
+int attendre_connexion(int sockfd) {
+    int newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+    if (newsockfd < 0) {
+        traiter_erreur("Erreur lors de l'acceptation de la connexion");
     }
-
-    return socktraitement;
+    return newsockfd;
 }
 
-void recevoir_message(int socktraitement, char * buffer) {
-    int n;
-    n = recv(socktraitement, buffer, 1024, 0);
-    if (n == -1) {
-        traiter_erreur(__FUNCTION__);
+void recevoir_message(int sockfd, char *buffer) {
+    ssize_t nbytes = read(sockfd, buffer, TAILLE_MSG - 1);
+    if (nbytes < 0) {
+        traiter_erreur("Erreur lors de la réception du message");
     }
-    buffer[n] = '\0'; 
+    buffer[nbytes] = '\0'; // Assurer que le message est terminé par un caractère nul
 }
 
-void envoyer_message(int socktraitement, char * message) {
-    int n;
-    n = send(socktraitement, message, strlen(message), 0);
-    if (n == -1) {
-        traiter_erreur(__FUNCTION__);
+void envoyer_message(int sockfd, char *message) {
+    ssize_t nbytes = write(sockfd, message, strlen(message));
+    if (nbytes < 0) {
+        traiter_erreur("Erreur lors de l'envoi du message");
     }
 }
 
-void fermer_connexion(int socktraitement) {
-    if (close(socktraitement) == -1) {
-        traiter_erreur(__FUNCTION__);
+void fermer_connexion(int sockfd) {
+    if (close(sockfd) < 0) {
+        traiter_erreur("Erreur lors de la fermeture de la connexion");
     }
 }
