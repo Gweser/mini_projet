70b49e7877529772ce1fdec7de8cca7759fcd70f
SEVENO Enzo & RIOU LE DROGO Dylan <seveno.e@stjolorient.fr>
2025-02-03 11:12:28 +0100

    Modification du tcp.h

--- TCP/tcp.h
+++ TCP/tcp.h
@@ -1,16 +1,13 @@
-#include <sys/socket.h>
-#include "erreur.h"
-
 #ifndef _TCP_H
 #define _TCP_H
 #define TAILLE_MSG 256
 
 int creer_socket(char*, int);
 void connecter_socket(int);
-void attacher_socket();
-void dimensionner_file_attente_socket(int);
+void attacher_socket(int);
+void dimensionner_file_attente_socket(int, int);
 void init_addr_client();
-int attendre_connexion();
+int attendre_connexion(int);
 void recevoir_message(int, char *);
 void envoyer_message(int, char *);
 void fermer_connexion(int);
